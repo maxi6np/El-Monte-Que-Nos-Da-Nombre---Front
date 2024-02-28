a5624bded36b62bc4f825e30ca148b5bddf9e6b0
Pablo <wzi21116@educastur.es>
2024-02-28 13:03:22 +0100

    barra mapas

--- src/MapaPuntos.jsx
+++ src/MapaPuntos.jsx
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import {
+    AppBar,
+    Box,
+    Button,
+    Container,
+    Grid,
+    Toolbar,
+    Typography,
+  } from "@mui/material";
+  import { Link } from "react-router-dom";
+  import PersonIcon from "@mui/icons-material/Person";
+  import LogoFinal from './img/logo_final.png';
+  import AspectRatio from '@mui/joy/AspectRatio';
+  import { Cookies, useCookies } from 'react-cookie';
+  import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+  
 
 function MapaPuntos() {
-    const [puntos, setPuntos] = useState(null)
+    const [puntos, setPuntos] = useState([])
+    const [cookies, setCookie, removeCookie] = useCookies(['session']);
 
     useEffect(() => {
         const map = L.map('map').setView([43.3736, -5.8500], 13);
@@ -19,14 +36,15 @@ function MapaPuntos() {
             popupAnchor: [0, -35]
         });
 
-        fetch('http://127.0.0.1:8000/mapa-puntos')
+        fetch('http://127.0.0.1:8000/mapa-puntos', { method: 'get' })
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 setPuntos(data);
+                console.log(puntos)
 
                 // Iterar sobre puntos aquí, después de que puntos esté definido
-                data.forEach(punto => {
+                puntos.forEach(punto => {
                     marker = L.marker([punto.latitud, punto.longitud]).addTo(map)
                         .bindPopup(`<b>${punto.nombre}</b>`);
                     marker.on('click', function (e) {
@@ -34,9 +52,9 @@ function MapaPuntos() {
                     });
                 });
             })
-            .catch(error => {
-                console.error('Error en la solicitud fetch:', error);
-            });
+        // .catch(error => {
+        //     console.error('Error en la solicitud fetch:', error);
+        // });
 
         map.on('contextmenu', () => {
             map.setView([40, -3.7], 6);
@@ -52,7 +70,83 @@ function MapaPuntos() {
     }, []); // Run this effect only once on mount
 
     return (
-        <div id="map" style={{ width: '80%', height: '80vh' }}></div>
+        <>
+            <AppBar position="static">
+                <Toolbar sx={{ width: "100vw" }}>
+                    <Grid2 container sx={{ width: "100%" }}>
+                        <Grid2 xs display="flex" justifyContent="start" alignItems="center">
+                            <AspectRatio
+                                className="mt-2 mb-2"
+                                variant="plain"
+                                sx={{
+                                    flexBasis: "200px",
+                                    overflow: "auto",
+                                    justifySelf: "start",
+                                }}
+                            >
+                                <img src={LogoFinal} alt="IES MONTE NARANCO" />
+                            </AspectRatio>
+                        </Grid2>
+                        <Grid2
+                            xs
+                            display="flex"
+                            justifyContent="center"
+                            alignItems="center"
+                        >
+                            <Typography
+                                variant="h6"
+                                component="div"
+                                sx={{ textAlign: "center" }}
+                            >
+                                El Monte que nos da el nombre
+                            </Typography>
+                        </Grid2>
+                        <Grid2 xs display="flex" justifyContent="end" alignItems="center">
+                            <Box component="div">
+                                <PersonIcon />
+                                {cookies.session ? (
+                                    <Button
+                                        color="inherit"
+                                        onClick={() => logout()}
+                                        component={Link}
+                                        to="/"
+                                    >
+                                        Logout
+                                    </Button>
+                                ) : (
+                                    <Button color="inherit" component={Link} to="/login">
+                                        Login
+                                    </Button>
+                                )}
+                                {!cookies.session && (
+                                    <Button color="inherit" component={Link} to="/registro">
+                                        Registrarse
+                                    </Button>
+                                )}
+                            </Box>
+                        </Grid2>
+                    </Grid2>
+                </Toolbar>
+            </AppBar>
+            <AppBar position="static" sx={{ backgroundColor: "#004d40" }}>
+                <Toolbar>
+                    <Button color="inherit" component={Link} to="/">
+                        Inicio
+                    </Button>
+                    <Button color="inherit" component={Link} to="/mapa-puntos">
+                        Descubre
+                    </Button>
+                    <Button color="inherit" component={Link} to="/itinerarios">
+                        Itinerarios
+                    </Button>
+                    <Button color="inherit" component={Link} to="/informacion">
+                        Información del Proyecto
+                    </Button>
+                </Toolbar>
+            </AppBar>
+
+            <div id="map" style={{ width: '80%', height: '80vh' }}></div>
+        </>
     );
 }
 
