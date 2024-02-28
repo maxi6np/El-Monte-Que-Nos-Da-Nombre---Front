69f1290793178ff9bca2e80571aaefd16a135b2f
YudithL <txz91117@educastur.es>
2024-02-28 11:41:30 +0100

    Cambios inicio

--- src/inicio.jsx
+++ src/inicio.jsx
@@ -1,28 +1,109 @@
-import {
-  AppBar,
-  Button,
-  Container,
-  Grid,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import React from "react";
-import { Link } from "react-router-dom";
 import PersonIcon from "@mui/icons-material/Person";
+import { AppBar, Box, Button, Grid, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import { useCookies } from "react-cookie";
+import { Link } from "react-router-dom";
 
 const Inicio = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["session"]);
+
+  const logout = () => {
+    fetch("http://127.0.0.1:8000/logout", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+        Authorization: "Bearer " + cookies.session.token,
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        removeCookie("session");
+      });
+  };
   return (
     <div>
-      
-      <Container>
+      <AppBar position="static" sx={{ backgroundColor: "#004d40" }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: "center" }}
+          >
+            El Monte que nos da el nombre
+          </Typography>
+          <PersonIcon />
+          {cookies.session ? (
+            <Button
+              color="inherit"
+              onClick={() => logout()}
+              component={Link}
+              to="/"
+            >
+              Logout
+            </Button>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+          )}
+          {!cookies.session && (
+            <Button color="inherit" component={Link} to="/registro">
+              Registrarse
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+      <AppBar position="static" sx={{ backgroundColor: "#004d40" }}>
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">
+            Inicio
+          </Button>
+          <Button color="inherit" component={Link} to="/descubre">
+            Descubre
+          </Button>
+          <Button color="inherit" component={Link} to="/itinerarios">
+            Itinerarios
+          </Button>
+          <Button color="inherit" component={Link} to="/informacion">
+            Información del Proyecto
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Box sx={{ backgroundColor: "#F5FCF8", py: 4 }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <Typography variant="h4" align="center" gutterBottom>
-              Lugares que puedes visitar
+              "El Naranco que da para todo"
+            </Typography>
+            <Typography variant="h4" align="center" gutterBottom>
+              M. A. Macía
             </Typography>
           </Grid>
         </Grid>
-      </Container>
+      </Box>
+
+      <Box sx={{ backgroundColor: "#a5d6a7", py: 4 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center" gutterBottom>
+              Itinerarios
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box sx={{ backgroundColor: "#c8e6c9", py: 4 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center" gutterBottom>
+              Información del Proyecto
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 };
