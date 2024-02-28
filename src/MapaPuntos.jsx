83ee32366d02964ddb4cd105bb9e0d6b8f25a6b4
Pablo <wzi21116@educastur.es>
2024-02-28 14:13:04 +0100

    falta onContextMenu

--- src/MapaPuntos.jsx
+++ src/MapaPuntos.jsx
@@ -9,63 +9,61 @@ import {
     Grid,
     Toolbar,
     Typography,
-  } from "@mui/material";
-  import { Link } from "react-router-dom";
-  import PersonIcon from "@mui/icons-material/Person";
-  import LogoFinal from './img/logo_final.png';
-  import AspectRatio from '@mui/joy/AspectRatio';
-  import { Cookies, useCookies } from 'react-cookie';
-  import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-  
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import PersonIcon from "@mui/icons-material/Person";
+import LogoFinal from './img/logo_final.png';
+import AspectRatio from '@mui/joy/AspectRatio';
+import { Cookies, useCookies } from 'react-cookie';
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import Markers from './Markers';
+
 
 function MapaPuntos() {
     const [puntos, setPuntos] = useState([])
     const [cookies, setCookie, removeCookie] = useCookies(['session']);
-
+    const [zoomLevel, setZoomLevel] = useState(13);
+    const [Latlong, setLatlong] = useState([43.3736, -5.8500])
+    const cambiarZoom = (e) => {
+        setLatlong(e.getLatLng()); 
+        setZoomLevel(20)
+    }
     useEffect(() => {
-        const map = L.map('map').setView([43.3736, -5.8500], 13);
-        const tiles = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
 
-        let marker = null;
-        const myIcon = L.icon({
-            iconUrl: './img/balon.svg',
-            iconSize: [35, 35],
-            iconAnchor: [17, 35],
-            popupAnchor: [0, -35]
-        });
+        
+
+
+
 
         fetch('http://127.0.0.1:8000/mapa-puntos', { method: 'get' })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                setPuntos(data);
-                console.log(puntos)
+                .then(response => response.json())
+             .then(data => setPuntos(data))
+                 
 
-                // Iterar sobre puntos aquí, después de que puntos esté definido
-                puntos.forEach(punto => {
-                    marker = L.marker([punto.latitud, punto.longitud]).addTo(map)
-                        .bindPopup(`<b>${punto.nombre}</b>`);
-                    marker.on('click', function (e) {
-                        map.setView(this.getLatLng(), 20);
-                    });
-                });
-            })
-        // .catch(error => {
-        //     console.error('Error en la solicitud fetch:', error);
+        //         // Iterar sobre puntos aquí, después de que puntos esté definido
+        //         puntos.forEach(punto => {
+        //             marker = L.marker([punto.latitud, punto.longitud]).addTo(map)
+        //                 .bindPopup(`<b>${punto.nombre}</b>`);
+        //             marker.on('click', function (e) {
+        //                 map.setView(this.getLatLng(), 20);
+        //             });
+        //         });
+        //     })
+        // // .catch(error => {
+        // //     console.error('Error en la solicitud fetch:', error);
+        // // });
+
+        // map.on('contextmenu', () => {
+        //     map.setView([40, -3.7], 6);
+        //     map.closePopup();
         // });
 
-        map.on('contextmenu', () => {
-            map.setView([40, -3.7], 6);
-            map.closePopup();
-        });
+        // // Clean up function
+        // return () => {
+        //     map.off(); // Remove all event listeners
+        //     map.remove(); // Remove the map instance
 
-        // Clean up function
-        return () => {
-            map.off(); // Remove all event listeners
-            map.remove(); // Remove the map instance
-        };
 
     }, []); // Run this effect only once on mount
 
@@ -144,8 +142,15 @@ function MapaPuntos() {
                     </Button>
                 </Toolbar>
             </AppBar>
+            <MapContainer center={Latlong} zoom={zoomLevel} scrollWheelZoom={false} style={{height:'80vh', width:'80%'}}>
+                <TileLayer 
+                    attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}"
+                />
+               <Markers puntos={puntos}></Markers>
+                
+            </MapContainer>
 
-            <div id="map" style={{ width: '80%', height: '80vh' }}></div>
         </>
     );
 }
