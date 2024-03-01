8e47d21564aec476acc8c9a58e4691f7ecca7879
Pablo <WZI21116@educastur.es>
2024-03-01 22:50:41 +0100

    Arreglo fetch

--- src/Itinerarios/Itinerarios.jsx
+++ src/Itinerarios/Itinerarios.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 
- function Itinerarios(){
-const [rutas, setRutas] = useState([]);
 
-    useEffect(()=>{
-        fetch('http://127.0.0.1:8000/get-rutas', {method:'get'})
+
+function Itinerarios() {
+    const [rutas, setRutas] = useState([]);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/get-rutas', { method: 'get' })
             .then(response => response.json())
-            .then(data => {setRutas(data.data); console.log(rutas)});
+            .then(data => setRutas(data.data));
     }, []);
- }
 
 
- export default Itinerarios
+
+}
+
+
+export default Itinerarios
