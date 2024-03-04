4064025e456e5fa6f24e687e6834c46a57c4a72a
Pablo <wzi21116@educastur.es>
2024-03-04 14:15:12 +0100

    Ordenacion Rutas

--- src/Itinerarios/Rutas.jsx
+++ src/Itinerarios/Rutas.jsx
@@ -11,35 +11,66 @@ import DescriptionIcon from '@mui/icons-material/Description';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import StarIcon from '@mui/icons-material/Star';
 import { useCookies } from 'react-cookie';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function Rutas() {
     const [rutas, setRutas] = useState([]);
-    const[cookies, setCookie, removeCookie] = useCookies('session');
+    const [cookies, setCookie, removeCookie] = useCookies('session');
+    const [ordenarPor, setOrdenarPor] = useState();
 
+    const Ordenar = (condicion) => {
+        switch (condicion){
+            case 'Reciente':
+                setRutas(rutas.sort((a, b) => new Date(b.fecha_creacion) - new Date(a.fecha_creacion)));
+                break;
+            case 'Longitud':
+                setRutas(rutas.sort((a, b) => b.duracion - a.duracion ));
+                break;
+        }
+    }
     useEffect(() => {
         let body = JSON.stringify({
             token: (cookies.session ? cookies.session.token : '')
         })
-        fetch('http://127.0.0.1:8000/get-rutas', { method: "post",
-        body:body,
-        headers: {
-          Accept: "application/json",
-          "Content-type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        }, })
+        fetch('http://127.0.0.1:8000/get-rutas', {
+            method: "post",
+            body: body,
+            headers: {
+                Accept: "application/json",
+                "Content-type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+        })
             .then(response => response.json())
             .then(data => setRutas(data.data));
     }, []);
 
     return (
         <>
+            <FormControl  sx={{marginBottom:'2rem', width:'30%'}}>
+                <InputLabel id="select-ordenacion-label">Ordenar por</InputLabel>
+                <Select
+                    labelId="select-ordenacion-label"
+                    id="select-ordenacion"
+                    label="Ordenar por"
+                    value={ordenarPor}
+                    onChange={(e) => {setOrdenarPor(e.target.value); Ordenar(e.target.value);}}
+                >
+                    <MenuItem value={'Reciente'}>Reciente</MenuItem>
+                    <MenuItem value={'%completada'}>%completada</MenuItem>
+                    <MenuItem value={'Longitud'}>Longitud</MenuItem>
+                </Select>
+            </FormControl>
             {rutas.map((ruta) => (
                 <Card sx={{
                     width: '50vw', marginBottom: '2rem', border: '1px solid #b8bec2',
                     borderRadius: '8px',
                     boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)'
                 }}>
-                    <CardActionArea>
+                    <CardActionArea component='span'>
                         <CardContent>
                             <Grid container spacing={2}>
                                 <Grid item xs={12} md={5}>
@@ -58,7 +89,7 @@ export default function Rutas() {
                                         <p><StarIcon /> Dificultad: {ruta.dificultad}</p>
                                         <p><AccessTimeIcon /> Duración: {ruta.duracion}h</p>
                                         <p><DescriptionIcon /> Descripción: {ruta.descripcion}</p>
-                                        <Button variant="contained" color="primary" sx={{ backgroundColor: '#00897b', marginTop:'1rem' }}>
+                                        <Button variant="contained" color="primary" sx={{ backgroundColor: '#00897b', marginTop: '1rem' }}>
                                             Ver detalles
                                         </Button>
                                     </Typography>
