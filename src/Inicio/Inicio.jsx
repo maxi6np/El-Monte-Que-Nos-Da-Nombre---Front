0e00914c835134028ebe8dfcad280147bd6194a9
YudithL <txz91117@educastur.es>
2024-03-11 14:03:10 +0100

    Responsive

--- src/Inicio/Inicio.jsx
+++ src/Inicio/Inicio.jsx
@@ -1,6 +1,7 @@
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
 import PersonIcon from "@mui/icons-material/Person";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import { AppBar, Box, Button, Grid, Toolbar, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import { React } from "react";
@@ -11,178 +12,226 @@ import icono_movil2 from "../img/icono_movil2.png";
 import LogoFinalBanner from "../img/logo_final_Banner.png";
 import Cita from "./Cita";
 import Video from "./Video";
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 const Inicio = ({ logout, activeButton, setActiveButton }) => {
   const [cookies, setCookie, removeCookie] = useCookies(["session"]);
 
   return (
     <div>
-      <AppBar position="static" sx={{ backgroundColor: "#004d40" }}>
-        <Toolbar sx={{ width: "100%" }}>
-          <Grid2 container sx={{ width: "100%" }}>
-            <Grid2 xs display="flex" justifyContent="start" alignItems="center">
-              <Box
-                component="div"
-                sx={{
-                  flexBasis: "100px",
-                  justifySelf: "start",
-                  marginTop: "1rem",
-                  marginBottom: "1rem",
-                }}
-              >
-                <Link to="/">
-                  {" "}
-                  <img
-                    src={LogoFinalBanner}
-                    alt="IES MONTE NARANCO"
-                    style={{ height: "100%", width: "100%" }}
-                    onClick={() => setActiveButton("Inicio")}
-                  />
-                </Link>
-              </Box>
-            </Grid2>
-            <Grid2
-              xs
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <Typography
-                variant="h5"
-                component="div"
-                sx={{ textAlign: "center" }}
-              >
-                EL MONTE QUE NOS DA NOMBRE
-              </Typography>
-            </Grid2>
-            <Grid2 xs display="flex" justifyContent="end" alignItems="center">
-              <Box component="div" display="flex" justifyContent="end" alignItems="center">
-
-                {cookies.session && (
-                  <Box component="div" display="flex" justifyContent="end" alignItems="center" flexWrap="wrap" gap="0.5rem"> 
-                    <PersonIcon ></PersonIcon>
-                    <Typography
-                    
-                      
-                      sx={{ marginRight: '1rem', textAlign: 'center'}}
-                    >
-                      
-                      {cookies.session.username.toUpperCase()}
-                    </Typography>
-                  </Box>
-                )}
-                {cookies.session ? (
-
-
-                  <Button
-                    color="inherit"
-                    onClick={() => {
-                      setActiveButton("Inicio");
-                      logout();
+      <Grid container spacing={2}>
+        <Grid item sm={12} md={12} lg={12}>
+          <AppBar position="static" sx={{ backgroundColor: "#004d40" }}>
+            <Toolbar sx={{ width: "100%" }}>
+              <Grid2 container sx={{ width: "100%" }}>
+                <Grid2
+                  item
+                  sm={2}
+                  md={2}
+                  lg={4}
+                  display="flex"
+                  justifyContent="start"
+                  alignItems="center"
+                >
+                  <Box
+                    component="div"
+                    sx={{
+                      flexBasis: "100px",
+                      justifySelf: "start",
+                      marginTop: "1rem",
+                      marginBottom: "1rem",
+                      marginLeft: "1rem",
                     }}
-                    component={Link}
-                    to="/"
-                    startIcon={<LogoutIcon></LogoutIcon>}
-                    sx={{ ":hover": { backgroundColor: "#00897b" } }}
+                    item
                   >
-                    Cerrar Sesión
-                  </Button>
+                    <Link to="/">
+                      {" "}
+                      <img
+                        src={LogoFinalBanner}
+                        alt="IES MONTE NARANCO"
+                        style={{ height: "100%", width: "100%" }}
+                        onClick={() => setActiveButton("Inicio")}
+                      />
+                    </Link>
+                  </Box>
+                </Grid2>
+                <Grid2
+                  item
+                  sm={10}
+                  md={6}
+                  lg={4}
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Typography
+                    variant="h5"
+                    component="div"
+                    sx={{ textAlign: "center" }}
+                  >
+                    EL MONTE QUE NOS DA NOMBRE
+                  </Typography>
+                </Grid2>
+                <Grid2
+                  item
+                  sm={9}
+                  md={4}
+                  lg={4}
+                  display="flex"
+                  justifyContent="end"
+                  alignItems="center"
+                >
+                  <Box
+                    component="div"
+                    display="flex"
+                    justifyContent="end"
+                    alignItems="center"
+                  >
+                    {cookies.session && (
+                      <Box
+                        component="div"
+                        display="flex"
+                        justifyContent="end"
+                        alignItems="center"
+                        flexWrap="wrap"
+                        gap="0.5rem"
+                      >
+                        <PersonIcon></PersonIcon>
+                        <Typography
+                          sx={{ marginRight: "1rem", textAlign: "center" }}
+                        >
+                          {cookies.session.username.toUpperCase()}
+                        </Typography>
+                      </Box>
+                    )}
+                    {cookies.session ? (
+                      <Button
+                        color="inherit"
+                        onClick={() => {
+                          setActiveButton("Inicio");
+                          logout();
+                        }}
+                        component={Link}
+                        to="/"
+                        startIcon={<LogoutIcon></LogoutIcon>}
+                        sx={{ ":hover": { backgroundColor: "#00897b" } }}
+                      >
+                        Cerrar Sesión
+                      </Button>
+                    ) : (
+                      <Button
+                        color="inherit"
+                        component={Link}
+                        to="/login"
+                        startIcon={<LoginIcon></LoginIcon>}
+                        sx={{ ":hover": { backgroundColor: "#00897b" } }}
+                      >
+                        Iniciar Sesión
+                      </Button>
+                    )}
 
-                ) : (
-                  <Button
-                    color="inherit"
-                    component={Link}
-                    to="/login"
-                    startIcon={<LoginIcon></LoginIcon>}
-                    sx={{ ":hover": { backgroundColor: "#00897b" } }}
-                  >
-                    Iniciar Sesión
-                  </Button>
-                )}
-                {!cookies.session && (
-                  <Button
-                    color="inherit"
-                    component={Link}
-                    to="/registro"
-                    startIcon={<PersonAddIcon></PersonAddIcon>}
-                    sx={{ ":hover": { backgroundColor: "#00897b" } }}
-                  >
-                    Registrarse
-                  </Button>
-                )}
-              </Box>
-            </Grid2>
-          </Grid2>
-        </Toolbar>
-      </AppBar>
-      <AppBar position="static" sx={{ backgroundColor: "#004d40" }}>
-        <Toolbar sx={{ height: "100%" }}>
-          <Button
-            color="inherit"
-            component={Link}
-            to="/"
-            sx={{
-              height: "100%",
-              ":hover": {
-                backgroundColor: activeButton === "Inicio" ? "#00897b" : null,
-              },
-              backgroundColor: activeButton === "Inicio" ? "#00897b" : null,
-            }}
-            onClick={() => setActiveButton("Inicio")}
-          >
-            Inicio
-          </Button>
-          <Button
-            color="inherit"
-            component={Link}
-            to="/descubre"
-            sx={{
-              ":hover": {
-                backgroundColor: activeButton === "Descubre" ? "#00897b" : null,
-              },
-              backgroundColor: activeButton === "Descubre" ? "#00897b" : null,
-            }}
-            onClick={() => setActiveButton("Descubre")}
-          >
-            Descubre
-          </Button>
-          <Button
-            color="inherit"
-            component={Link}
-            to="/itinerarios"
-            sx={{
-              ":hover": {
-                backgroundColor:
-                  activeButton === "Itinerarios" ? "#00897b" : null,
-              },
-              backgroundColor:
-                activeButton === "Itinerarios" ? "#00897b" : null,
-            }}
-            onClick={() => setActiveButton("Itinerarios")}
-          >
-            Itinerarios
-          </Button>
-          {cookies.session && (
-            <Button
-              color="inherit"
-              component={Link}
-              to="/planificar"
+                    {!cookies.session && (
+                      <Button
+                        color="inherit"
+                        component={Link}
+                        to="/registro"
+                        startIcon={<PersonAddIcon></PersonAddIcon>}
+                        sx={{ ":hover": { backgroundColor: "#00897b" } }}
+                      >
+                        Registrarse
+                      </Button>
+                    )}
+                  </Box>
+                </Grid2>
+              </Grid2>
+            </Toolbar>
+          </AppBar>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2}>
+        <Grid item sm={12} md={12}>
+          <AppBar position="static" sx={{ backgroundColor: "#004d40" }}>
+            <Toolbar
               sx={{
-                ":hover": {
-                  backgroundColor:
-                    activeButton === "Planificar" ? "#00897b" : null,
+                height: "100%",
+                marginLeft: "1rem",
+                justifyContent: {
+                  sm: "flex-start",
+                  md: "flex-start",
+                  lg: "flex-start",
                 },
-                backgroundColor:
-                  activeButton === "Planificar" ? "#00897b" : null,
               }}
-              onClick={() => setActiveButton("Planificar")}
             >
-              Planificar ruta
-            </Button>
-          )}
-        </Toolbar>
-      </AppBar>
+              <Button
+                color="inherit"
+                component={Link}
+                to="/"
+                sx={{
+                  height: "100%",
+                  ":hover": {
+                    backgroundColor:
+                      activeButton === "Inicio" ? "#00897b" : null,
+                  },
+                  backgroundColor: activeButton === "Inicio" ? "#00897b" : null,
+                }}
+                onClick={() => setActiveButton("Inicio")}
+              >
+                Inicio
+              </Button>
+              <Button
+                color="inherit"
+                component={Link}
+                to="/descubre"
+                sx={{
+                  ":hover": {
+                    backgroundColor:
+                      activeButton === "Descubre" ? "#00897b" : null,
+                  },
+                  backgroundColor:
+                    activeButton === "Descubre" ? "#00897b" : null,
+                }}
+                onClick={() => setActiveButton("Descubre")}
+              >
+                Descubre
+              </Button>
+              <Button
+                color="inherit"
+                component={Link}
+                to="/itinerarios"
+                sx={{
+                  ":hover": {
+                    backgroundColor:
+                      activeButton === "Itinerarios" ? "#00897b" : null,
+                  },
+                  backgroundColor:
+                    activeButton === "Itinerarios" ? "#00897b" : null,
+                }}
+                onClick={() => setActiveButton("Itinerarios")}
+              >
+                Itinerarios
+              </Button>
+              {cookies.session && (
+                <Button
+                  color="inherit"
+                  component={Link}
+                  to="/planificar"
+                  sx={{
+                    ":hover": {
+                      backgroundColor:
+                        activeButton === "Planificar" ? "#00897b" : null,
+                    },
+                    backgroundColor:
+                      activeButton === "Planificar" ? "#00897b" : null,
+                  }}
+                  onClick={() => setActiveButton("Planificar")}
+                >
+                  Planificar ruta
+                </Button>
+              )}
+            </Toolbar>
+          </AppBar>
+        </Grid>
+      </Grid>
 
       <Cita />
       <Video />
